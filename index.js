778341e89157883e7768fd65098e26c20e24f744
Andrew Stanish <andrew.stanish@nbcuni.com>
2018-01-29 18:24:38 -0500

    Revert "es5"
    This reverts commit bd4795e892e26caec7123bc7c36c66a4564b5658.

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 'use strict';
-var ipRegex = require('ip-regex');
-var tlds = require('tlds');
+const ipRegex = require('ip-regex');
+const tlds = require('tlds');
 
-module.exports = function (opts) {
+module.exports = opts => {
 	opts = Object.assign({strict: true}, opts);
 
-	var protocol = `(?:(?:[a-z]+:)?//)${opts.strict ? '' : '?'}`;
-	var auth = '(?:\\S+(?::\\S*)?@)?';
-	var ip = ipRegex.v4().source;
-	var host = '(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)';
-	var domain = '(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*';
-	var tld = `(?:\\.${opts.strict ? '(?:[a-z\\u00a1-\\uffff]{2,})' : `(?:${tlds.sort((a, b) => b.length - a.length).join('|')})`})\\.?`;
-	var port = '(?::\\d{2,5})?';
-	var path = '(?:[/?#][^\\s"]*)?';
-	var regex = `(?:${protocol}|www\\.)${auth}(?:localhost|${ip}|${host}${domain}${tld})${port}${path}`;
+	const protocol = `(?:(?:[a-z]+:)?//)${opts.strict ? '' : '?'}`;
+	const auth = '(?:\\S+(?::\\S*)?@)?';
+	const ip = ipRegex.v4().source;
+	const host = '(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)';
+	const domain = '(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*';
+	const tld = `(?:\\.${opts.strict ? '(?:[a-z\\u00a1-\\uffff]{2,})' : `(?:${tlds.sort((a, b) => b.length - a.length).join('|')})`})\\.?`;
+	const port = '(?::\\d{2,5})?';
+	const path = '(?:[/?#][^\\s"]*)?';
+	const regex = `(?:${protocol}|www\\.)${auth}(?:localhost|${ip}|${host}${domain}${tld})${port}${path}`;
 
 	return opts.exact ? new RegExp(`(?:^${regex}$)`, 'i') : new RegExp(regex, 'ig');
 };
