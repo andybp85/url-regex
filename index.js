63e621f576a0ccc7e61a4468f54e1bbba65d09bd
Andrew Stanish <andrew.stanish@nbcuni.com>
2018-01-29 20:32:47 -0500

    template strings

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ var tlds = require('tlds');
 module.exports = function (opts) {
 	opts = Object.assign({strict: true}, opts);
 
-	var protocol = `(?:(?:[a-z]+:)?//)${opts.strict ? '' : '?'}`;
+	var protocol = '(?:(?:[a-z]+:)?//)' + opts.strict ? '' : '?';
 	var auth = '(?:\\S+(?::\\S*)?@)?';
 	var ip = ipRegex.v4().source;
 	var host = '(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)';
 	var domain = '(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*';
-	var tld = `(?:\\.${opts.strict ? '(?:[a-z\\u00a1-\\uffff]{2,})' : `(?:${tlds.sort((a, b) => b.length - a.length).join('|')})`})\\.?`;
+	var tld = '(?:\\.' + opts.strict ? '(?:[a-z\\u00a1-\\uffff]{2,})' : '(?:' + tlds.sort((a, b) => b.length - a.length).join('|') + ')\\.?';
 	var port = '(?::\\d{2,5})?';
 	var path = '(?:[/?#][^\\s"]*)?';
-	var regex = `(?:${protocol}|www\\.)${auth}(?:localhost|${ip}|${host}${domain}${tld})${port}${path}`;
+	var regex = '(?:' + protocol + '|www\\.)' + auth + '(?:localhost|' + ip + '|' + host + domain + tld + ')' + port + path;
 
-	return opts.exact ? new RegExp(`(?:^${regex}$)`, 'i') : new RegExp(regex, 'ig');
+	return opts.exact ? new RegExp('(?:^' + regex + '$)', 'i') : new RegExp(regex, 'ig');
 };
